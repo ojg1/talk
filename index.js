b27e3a5a9f3c1647ebce6f99ae7fd83e7b980aa6
OrangeJuiceGuy1 <ericlin1253@gmail.com>
2026-04-13 20:48:46 -0400

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+
+document.getElementById("sendButton").addEventListener("click", function() {
+
+    let message = document.getElementById("sendInput").value;
+    document.getElementById("sendInput").value = "";
+})
