ff218982e6aca8d39f180b6ae532367a20c23370
OrangeJuiceGuy1 <ericlin1253@gmail.com>
2026-04-13 21:25:19 -0400

    post

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 
-document.getElementById("sendButton").addEventListener("click", function() {
+const ServerURL = "https://sku-wine-plenty-female.trycloudflare.com";
+
+document.getElementById("sendButton").addEventListener("click", async function() {
 
     let message = document.getElementById("sendInput").value;
     document.getElementById("sendInput").value = "";
+
+    if (message.trim() === "") return;
+
+    await fetch(ServerURL + "/message", {
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            text:message
+        })
+
+    });
+
 })
